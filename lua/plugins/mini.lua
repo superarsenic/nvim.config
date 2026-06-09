877d151905355b237d7dd3ee8aa59f419b1efb82
Arseniy <arseniy.sergeenko@kuori.tech>
2026-06-09 12:44:28 +0300

    removed annoying lsp progress from notify

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -8,4 +8,8 @@ require("mini.pairs").setup({})
 require("mini.trailspace").setup({})
 require("mini.bufremove").setup({})
 require("mini.icons").setup({})
-require("mini.notify").setup({})
+require("mini.notify").setup({
+	lsp_progress = {
+		enable = false,
+	},
+})
